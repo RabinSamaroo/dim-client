1ac8b1703e7b1afbc4620f3acd34387b4515fa3a
Rabin Samaroo <r1samaroo@ryerson.ca>
2021-12-18 03:41:19 +0000

    add git ignore

--- src/ActivityCard.tsx
+++ src/ActivityCard.tsx
@@ -1,7 +1,7 @@
 import {
   LocationMarkerIcon,
   ClockIcon,
-  CashIcon,
+  CalendarIcon,
 } from "@heroicons/react/solid";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,13 +11,10 @@ export default function ActivityCard({ activity }: any) {
   return (
     <div
       key={activity.ProgramId}
-      className="block  rounded-full border-indigo-100 border-2 mb-2 hover:bg-gray-50 hover:border-indigo-200"
+      className="block rounded-3xl border-indigo-100 border-2 m-2 hover:bg-gray-50 hover:border-indigo-200"
     >
-      <a
-        href={activity.Barcode}
-        className="block hover:bg-gray-50 rounded-full"
-      >
-        <div className="flex items-center px-4 py-4 sm:px-6 rounded-full">
+      <a href={activity.Barcode} className="block rounded-3xl">
+        <div className="flex items-center px-4 py-4 sm:px-6 rounded-3xl">
           <div className="min-w-0 flex-1 flex items-center">
             <div className="flex-shrink-0">
               {/* <img
@@ -28,22 +25,22 @@ export default function ActivityCard({ activity }: any) {
 
               <FontAwesomeIcon
                 icon={faSwimmer}
-                className="text-6xl mx-2 h-12 w-12 text-indigo-500"
+                className="text-4xl mx-2 h-12 w-12 text-indigo-500"
               ></FontAwesomeIcon>
             </div>
-            <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
+            <div className="min-w-0 flex-2 px-4 md:grid md:grid-cols-1 md:gap-4">
               <div>
                 <p className="text-sm font-medium text-indigo-600 truncate">
                   {activity.Title}
                 </p>
-                <p className="mt-2 flex items-center text-sm text-gray-500">
+                <p className="mt-2 flex items-center text-sm text-gray-500 truncate">
                   <LocationMarkerIcon
                     className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
                     aria-hidden="true"
                   />
                   <span className="truncate">{activity.LocationName}</span>
                 </p>
-                <p className="mt-2 flex items-center text-sm text-gray-500">
+                <p className="mt-2 flex items-center text-sm text-gray-500 truncate">
                   <ClockIcon
                     className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
                     aria-hidden="true"
@@ -52,17 +49,13 @@ export default function ActivityCard({ activity }: any) {
                     " to " +
                     activity.FriendlyEndDate}
                 </p>
-              </div>
-              <div className="flex-1">
-                <div>
-                  <p className="mt-2 flex items-center text-sm text-gray-500">
-                    <CashIcon
-                      className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    {activity.Amount}
-                  </p>
-                </div>
+                <p className="mt-2 flex items-center text-sm text-gray-500 truncate">
+                  <CalendarIcon
+                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  {activity.date}
+                </p>
               </div>
             </div>
           </div>
