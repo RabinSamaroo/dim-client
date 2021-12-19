cd8bc1c282140f30a1d7cd68d350d7ea786b4c1f
Rabin Samaroo <r1samaroo@ryerson.ca>
2021-12-19 02:28:53 +0000

    use different server data

--- src/ActivityCard.tsx
+++ src/ActivityCard.tsx
@@ -10,10 +10,10 @@ import { faSwimmer } from "@fortawesome/free-solid-svg-icons";
 export default function ActivityCard({ activity }: any) {
   return (
     <div
-      key={activity.ProgramId}
+      key={activity.url}
       className="block rounded-3xl border-indigo-100 border-2 m-2 hover:bg-gray-50 hover:border-indigo-200"
     >
-      <a href={activity.Barcode} className="block rounded-3xl">
+      <a href={activity.url} className="block rounded-3xl">
         <div className="flex items-center px-4 py-4 sm:px-6 rounded-3xl">
           <div className="min-w-0 flex-1 flex items-center">
             <div className="flex-shrink-0">
@@ -31,30 +31,30 @@ export default function ActivityCard({ activity }: any) {
             <div className="min-w-0 flex-2 px-4 md:grid md:grid-cols-1 md:gap-4">
               <div>
                 <p className="text-sm font-medium text-indigo-600 truncate">
-                  {activity.Title}
+                  {activity.title}
                 </p>
                 <p className="mt-2 flex items-center text-sm text-gray-500 truncate">
                   <LocationMarkerIcon
                     className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
                     aria-hidden="true"
                   />
-                  <span className="truncate">{activity.LocationName}</span>
-                </p>
-                <p className="mt-2 flex items-center text-sm text-gray-500 truncate">
-                  <ClockIcon
-                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                  {activity.FriendlyStartDate +
-                    " to " +
-                    activity.FriendlyEndDate}
+                  <span className="truncate">{activity.locationName}</span>
                 </p>
                 <p className="mt-2 flex items-center text-sm text-gray-500 truncate">
                   <CalendarIcon
                     className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
                     aria-hidden="true"
                   />
-                  {activity.date}
+                  {activity.dateFormatted}
+                </p>
+                <p className="mt-2 flex items-center text-sm text-gray-500 truncate">
+                  <ClockIcon
+                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  {activity.startTime +
+                    " to " +
+                    activity.endTime}
                 </p>
               </div>
             </div>
