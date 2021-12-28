c9f83b428a1de26e245ade652370f9c0e5d6c06d
Rabin Samaroo <r1samaroo@ryerson.ca>
2021-12-28 09:42:19 +0000

    added 2 cols for locations from sm to lg

--- src/FilterLocation.tsx
+++ src/FilterLocation.tsx
@@ -6,7 +6,7 @@ export default function FilterLocation({ filterLocations, locationFilterChangeHa
             <label className="block text-sm font-medium text-gray-700 mt-4">
                 Locations
             </label>
-            <fieldset>
+            <fieldset className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1">
                 {locations.map((location) =>
                     <div key={location.locationName}>
                         <legend className="sr-only">{location.locationName}</legend>
