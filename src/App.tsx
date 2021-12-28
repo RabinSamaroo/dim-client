34497373674d3a21e01e4a03294c84534bdec346
Rabin Samaroo <r1samaroo@ryerson.ca>
2021-12-28 23:22:02 +0000

    remove layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import Layout from './Layout'
+import Navbar from './Navbar';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
     <React.StrictMode>
-      <Layout />
+      <div className="min-h-full">
+        <Navbar />
+        <Content />
+        <Footer />
+      </div>
     </React.StrictMode>
   );
 }
