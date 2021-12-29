ec9e6c923fbf24451e384de6fee88b2a9c66e906
Rabin Samaroo <r1samaroo@ryerson.ca>
2021-12-29 03:03:17 +0000

    fix buttons

--- src/FilterClear.tsx
+++ src/FilterClear.tsx
@@ -2,7 +2,7 @@ export default function FilterClear({ filterClearHandler }: any) {
     return (
         <button
             type="button"
-            className="inline-flex items-center mt-4 px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-slate-300 bg-slate-700 hover:bg-slate-600 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-slate-200"
+            className="inline-flex items-center mt-4 px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-slate-300 bg-slate-800 hover:bg-slate-600 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-slate-200"
             onClick={filterClearHandler}
         >Clear Filter</button>
     )
