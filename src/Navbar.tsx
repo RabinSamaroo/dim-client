cfc4f9cac3d4b2fec6e58a95f57cc1568e209611
Rabin Samaroo <r1samaroo@ryerson.ca>
2021-12-31 16:50:08 +0000

    update navbar and footer

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,16 +1,5 @@
-import { Fragment } from "react";
-
-import { Popover, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
-// import { SearchIcon } from "@heroicons/react/solid";
-function classNames(...classes: any[]) {
-  return classes.filter(Boolean).join(" ");
-}
-let item = {
-  name: "Home",
-  href: "#",
-  current: false
-}
+import { faSwimmer } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Navbar() {
   return (<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -18,11 +7,12 @@ export default function Navbar() {
       <div className="p-4 my-2 rounded-xl">
         <nav className="flex space-x-4">
           <div className="flex-shrink-0 flex items-center">
-            <img
-              className="block h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-              alt="Workflow"
-            />
+            <div className="min-w-0 flex-shrink-0 h-12 w-14 flex items-center -scale-x-100">
+              <FontAwesomeIcon
+                icon={faSwimmer}
+                className="text-4xl h-12 w-14 text-slate-200 flex-1"
+              />
+            </div>
           </div>
 
           <div className="flex flex-1 items-center">
